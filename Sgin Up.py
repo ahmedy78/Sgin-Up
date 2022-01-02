bc44736798c2aecef1a6409cc8645fb1d41e772a
ahmedy78 <97000559+ahmedy78@users.noreply.github.com>
2022-01-03 00:05:57 +0200

    Add files via upload

---
+++ Sgin Up.py
@@ -0,0 +1,25 @@
+import os
+os.system ("clear")
+##############################
+n = input ("Enter Your username :")
+if n == "rd" :
+    print ("Welcome Mr :"+n)
+else:
+    r = input ("Please Try Agin >>> :")
+    if r == "rd" :
+        print ("Good Mr :"+r)
+    else:
+        print("Sory I dont You Can..! Mr :"+n)
+##############################################
+p = input ("Enter The Password : ")
+if p == "123" :
+    print ("Exlant ")
+    print ("Thank You For Sgin In ")
+else:
+     t = input ("Please Try Agin >>> :")
+     if t == "123" :
+          print("Exlant Work :"+n)
+          print ("Thank You For Sgin In : "+n)
+     else:
+          print("I Dont You Can Mr : "+n)
+                
